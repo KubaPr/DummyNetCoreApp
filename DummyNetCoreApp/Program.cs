28c9146b0caea86809fac8d0768aa73647cfd5bd
KubaPr <kuba.prokopiuk@gmail.com>
2019-06-23 22:19:39 +0200

    Add test

--- DummyNetCoreApp/Program.cs
+++ DummyNetCoreApp/Program.cs
@@ -19,6 +19,6 @@ namespace DummyNetCoreApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>();
     }
 }
