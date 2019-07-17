850d8a723208b30c88f4537f2c5ad77e2eeead19
KubaPr <kuba.prokopiuk@gmail.com>
2019-07-17 14:33:35 +0200

    Commiting failing test

--- DummyNetCoreAppTests/UnitTest1.cs
+++ DummyNetCoreAppTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace DummyNetCoreAppTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 1);
+            Assert.AreEqual(1, 0);
         }
     }
 }
